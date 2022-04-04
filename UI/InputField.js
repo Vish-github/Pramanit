cb17cc298791b86db7e02a756d8a180b3f810426
Riddhi Siddarkar <riddhisiddarkar@gmail.com>
2022-04-05 01:16:09 +0530

    MODIFY:fixing form issue on large devices

--- UI/InputField.js
+++ UI/InputField.js
@@ -1,16 +1,22 @@
-import { Grid, Typography } from "@mui/material";
+import {Grid, Typography} from "@mui/material";
 
 import TextField from "../src/components/Forms/FormUI/TextFieldWrapper";
 
 import styles from "../styles/FormUI.module.css";
 
-function InputField({ title = "", name = "", type = "", ...otherProps }) {
+function InputField({
+  title = "",
+  name = "",
+  type = "",
+  smwidth = 10,
+  ...otherProps
+}) {
   return (
-    <Grid item sm={10} marginBottom={1} xs={12}>
+    <Grid item sm={smwidth} marginBottom={1} xs={12}>
       <Typography className={styles.label}>{title}</Typography>
       <TextField
         name={name}
-        InputProps={{ className: styles.input }}
+        InputProps={{className: styles.input}}
         type={type}
         {...otherProps}
       />
