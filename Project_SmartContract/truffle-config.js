7b662996cb953e34944bd8fe8c3fc9fa926b313a
kedar_devs <kedard249.kd@gmail.com>
2022-05-10 12:18:40 +0530

    add:Make truffle global

--- Project_SmartContract/truffle-config.js
+++ Project_SmartContract/truffle-config.js
@@ -1,13 +1,27 @@
-
-
+require('dotenv').config()
+const WalletHandler=require('@truffle/hdwallet-provider')
+console.log(process.env.PRIVATE_KEY,process.env.INFURA_API_KEY)
 module.exports = {
 
   networks: {
-    development:{
-      host:"127.0.0.1",
-      port:7545,
-      network_id:"*"
-    }
+    rinkeby:{
+      networkCheckTimeout: 10000,
+      provider:()=>new WalletHandler({
+        privateKeys:['f7c4e680ae9ebc56fb5a315a053385712451a3713c870bf63a5e8da3d057005d'],
+        providerOrUrl:"https://rinkeby.infura.io/v3/7a6e9661a3de47a3ac41a17f4aaf64c6",
+        numberOfAddresses:1
+      }),
+      network_id:4,
+      gas:5500000,
+      confirmations:2,
+      timeoutBlock:2000,
+      skipDryRun:true
+    },
+    // development:{
+    //   host:"127.0.0.1",
+    //   port:7545,
+    //   network_id:"*"
+    // }
   },
   solc: {
     optimizer: {
