8e56f06651e4ad9dc69fe777d89c9c3a26a3a4c1
Shivam Naik <shivamnaik8239@gmail.com>
2022-05-11 19:13:34 +0530

    ADDED: schema for birth application

---
+++ models/BirthApplication.schema.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const birthApplicationSchema = new mongoose.Schema({
+  childFirstName: {
+    type: String,
+  },
+  childLastName: {
+    type: String,
+  },
+  fatherName: {
+    type: String,
+  },
+  motherName: {
+    type: String,
+  },
+
+  dateOfBirth: {
+    type: Date,
+  },
+
+  placeOfBirth: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  fatherNationality: {
+    type: String,
+  },
+  motherNationality: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+  grandFatherName: {
+    type: String,
+  },
+  grandMotherName: {
+    type: String,
+  },
+  muncipalityLocation: {
+    type: String,
+  },
+  fatherIdentityProof: {
+    type: String,
+  },
+  motherIdentityProof: {
+    type: String,
+  },
+  addressProof: {
+    type: String,
+  },
+  birthProof: {
+    type: String,
+  },
+});
+
+global.BirthApplication =
+  mongoose.models.BirthApplication ||
+  mongoose.model("BirthApplication", birthApplicationSchema);
+
+export default global.BirthApplication;
