f4052942ac3f91611cf2e0d6dc773da0729b2a42
Riddhi Siddarkar <riddhisiddarkar@gmail.com>
2022-05-21 16:26:26 +0530

    ADDED: routing to municipality details page

--- src/components/MunicipalityDashboard/MunicipalityDetails.jsx
+++ src/components/MunicipalityDashboard/MunicipalityDetails.jsx
@@ -1,16 +1,27 @@
 import Image from "next/image";
+import {useRouter} from "next/router";
 
 import styles from "../../../styles/MunicipalityDetails.module.css";
 import editicon from "../../../assets/svgs/edit.svg";
 
 function MunicipalityDetails() {
+  const router = useRouter();
   return (
     <>
       <div className={styles.municipality_details}>
         <p className={styles.municipality_details_heading}>
           Muncipality Details
         </p>
-        <Image src={editicon} alt="Edit" width={20} height={20} />
+        <Image
+          src={editicon}
+          alt="Edit"
+          width={20}
+          height={20}
+          onClick={() => {
+            router.push("/municipality_details");
+          }}
+          className={styles.editicon}
+        />
       </div>
       <div className={styles.municipality_address}>
         <p>Dr Pissurlekar Road</p>
