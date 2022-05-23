f98da388f1da2527bff0df3b3326e81bb8810723
kedar_devs <kedard249.kd@gmail.com>
2022-05-23 12:13:09 +0530

    Fix:Connect to Google Auth

--- pages/login.js
+++ pages/login.js
@@ -5,12 +5,21 @@ import FormBackground from "../layout/FormBackground";
 import styles from "../styles/UserLogin.module.css";
 
 import userlogin from "../assets/PRAMANIT/loginuser.png";
-
+import {useRouter} from "next/router";
 import UserLoginForm from "../src/components/Forms/LoginForm";
 import { signIn, signOut, useSession } from "next-auth/react";
 // import BtnLogin from "../src/components/BtnLogin";
 import React, { useEffect } from "react";
-function UserLogin({ getProviders, session }) {
+function UserLogin() {
+  const session=useSession()
+  const router = useRouter();
+
+  const setToken=()=>{
+    console.log('in HERE',session)
+    localStorage.setItem("pramanit", JSON.stringify(session));
+    // window.location('/userdashboard')
+    router.push("/userdashboard");
+  }
   return (
     <FormBackground pagetitle="User Login" image={userlogin}>
       <div>
@@ -19,8 +28,11 @@ function UserLogin({ getProviders, session }) {
           <UserLoginForm />
         </div>
         <div>
-          <button onClick={() => signIn("google",{callbackUrl:"http://localhost:3000/userdashboard"})}>Sign in with google</button>
+          <button onClick={() => signIn("google",{callbackUrl:"http://localhost:3000/login"})}>Sign in with google</button>
           {/* <BtnLogin provider={Providers.Google} /> */}
+          {session.data!=undefined?
+          setToken():<></> 
+        }
         </div>
         <div className={styles.bottomText}>
           <p className={styles.message}>
@@ -35,9 +47,9 @@ function UserLogin({ getProviders, session }) {
   );
 }
 
-// Login.getInitialProps = async (context) => {
+// UserLogin.getInitialProps = async (context) => {
 //   return {
-//     providers: await providers(context),
+//     // providers: await providers(context),
 //     session: await getSession(context),
 //   };
 // };
