7eb3df2392b533c35ec8397fcbc14a61896a272a
Vish-github <vishwaktharmiya@gmail.com>
2022-06-12 09:24:08 +0530

    update nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,7 @@ require("dotenv").config;
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 export default NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
+  // adapter: MongoDBAdapter(clientPromise),
   session: {
     strategy: "jwt",
   },
@@ -27,76 +27,77 @@ export default NextAuth({
   database: process.env.MONGO_URI,
   secret: process.env.JWTPRIVATEKEY,
 
-  // callbacks: {
-  //   async jwt({ token, account }) {
-  //     console.log("account is ", account);
-  //     // Persist the OAuth access_token to the token right after signin
-  //     if (account) {
-  //       token.accessToken = account.access_token;
-  //     }
-  //     return token;
-  //   },
-  //   async session({ session, token, user }) {
-  //     // Send properties to the client, like an access_token from a provider.
-  //     console.log("session is ", session);
-  //     console.log("user is ", session.user);
-  //     console.log("token is ", token.accessToken);
-  //     console.log("--------------------------------");
-  //     session.accessToken = token.accessToken;
-  //     const { name, eemail } = session.user;
-  //     let username = name;
-  //     console.log(name);
-  //     let email = session.user.email;
-  //     console.log(email);
-  //     const password = "GoogleAuth";
-  //     let birthTransactionId = " ";
-  //     let deathIpfsHash = " ";
-  //     let deathTransactionId = " ";
-  //     let birthIpfsHash = " ";
-  //     let accessToken = token.accessToken;
-  //     let resetToken = " ";
-
-  //     User.findOne({ email: email }, (err, user) => {
-  //       console.log("email is ", email);
-  //       if (err) {
-  //         console.log(err);
-  //       }
-  //       if (!user) {
-  //         const newUser = new User({
-  //           username,
-  //           password,
-  //           email,
-  //           birthIpfsHash,
-  //           birthTransactionId,
-  //           deathIpfsHash,
-  //           deathTransactionId,
-  //           accessToken,
-  //           resetToken,
-  //         });
-  //         newUser
-  //           .save()
-  //           .then((result) => {
-  //             console.log("user created successfully");
-  //             return session.user;
-  //           })
-  //           .catch((err) => {
-  //             console.log("user not created successfully");
-  //             return err;
-  //           });
-  //       } else if (user) {
-  //         console.log("User Already exists", user);
-  //       }
-  //     });
-  //   },
-  // },
-
   callbacks: {
-    session: async (session, user) => {
-      console.log("------------------------------------------------")
-      console.log("session is",session);
-      console.log("user is ",user);
-      session.userId = user;
+    // async jwt({ token, account }) {
+    //   console.log("account is ", account);
+    //   // Persist the OAuth access_token to the token right after signin
+    //   if (account) {
+    //     token.accessToken = account.access_token;
+    //   }
+    //   return token;
+    // },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      console.log("session is ", session);
+      console.log("user is ", session.user);
+      console.log("token is ", token.accessToken);
+      console.log("--------------------------------");
+      session.accessToken = token.accessToken;
+      const { name, eemail } = session.user;
+      let username = name;
+      console.log(name);
+      let email = session.user.email;
+      console.log(email);
+      const password = "GoogleAuth";
+      let birthTransactionId = " ";
+      let deathIpfsHash = " ";
+      let deathTransactionId = " ";
+      let birthIpfsHash = " ";
+      let accessToken = token.accessToken;
+      let resetToken = " ";
+
+      User.findOne({ email: email }, (err, user) => {
+        console.log("email is ", email);
+        if (err) {
+          console.log(err);
+        }
+        if (!user) {
+          const newUser = new User({
+            username,
+            password,
+            email,
+            birthIpfsHash,
+            birthTransactionId,
+            deathIpfsHash,
+            deathTransactionId,
+            accessToken,
+            resetToken,
+          });
+          newUser
+            .save()
+            .then((result) => {
+              console.log("user created successfully");
+              return session.user;
+            })
+            .catch((err) => {
+              console.log("user not created successfully");
+              return err;
+            });
+        } else if (user) {
+          console.log("User Already exists", user);
+        }
+      });
       return Promise.resolve(session);
     },
   },
+
+  // callbacks: {
+  //   session: async (session, user) => {
+  //     console.log("------------------------------------------------")
+  //     console.log("session is",session);
+  //     console.log("user is ",user);
+  //     session.userId = user;
+  //     return Promise.resolve(session);
+  //   },
+  // },
 });
