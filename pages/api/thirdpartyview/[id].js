8c1865cce262b196fddc6451bff1c86fb1a4273f
Riddhi Siddarkar <riddhisiddarkar@gmail.com>
2022-06-16 20:36:05 +0530

    MODIFY: removed redundant code

--- pages/api/thirdpartyview/[id].js
+++ pages/api/thirdpartyview/[id].js
@@ -1,7 +1,6 @@
 import connectDB from "../../../middleware/mongodb";
 import User from "../../../models/User.schema";
 import TimeBasedCertificateSchema from "../../../models/TimeBasedCertificate.schema";
-import axios from "axios";
 
 const handler = async (req, res) => {
   const {id} = req.query;
@@ -27,16 +26,6 @@ const handler = async (req, res) => {
       console.log("error", err);
       res.send("done");
     });
-  //   const data = await BirthApplication.findOne({applicant_id: id});
-  //   const emailGetter = await User.findOne({_id: id});
-  //   const email = emailGetter.email;
-  //   console.log(data);
-  //   if (data != undefined || data != null) {
-  //     let allData = {email, data};
-  //     return res.status(200).send({allData});
-  //   } else {
-  //     return res.status(404).send({error: "No data find"});
-  //   }
 };
 
 export default connectDB(handler);
