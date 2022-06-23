10ff0a7ea15b9a69ee512ae59a612b2d87e21979
Riddhi Siddarkar <riddhisiddarkar@gmail.com>
2022-06-24 02:32:41 +0530

    MODIFY: pdf format of birth certificate

--- pages/api/pdf_getter/[cid].js
+++ pages/api/pdf_getter/[cid].js
@@ -3,6 +3,8 @@ import connectDB from "../../../middleware/mongodb";
 import process from "process";
 import {Web3Storage, getFilesFromPath} from "web3.storage";
 import axios from "axios";
+import english from "./data/english";
+import konkani from "./data/konkani";
 // import poppins from "./fonts/Poppins.ttf";
 const pdfDoc = require("pdfkit");
 const path = require("path");
@@ -30,84 +32,168 @@ const pdfbuilder = async (data, res) => {
   const poppinsFont = fs.readFileSync(
     path.resolve("./", "./pages/api/pdf_getter/fonts/Poppins.ttf")
   );
+  const poppinsBoldFont = fs.readFileSync(
+    path.resolve("./", "./pages/api/pdf_getter/fonts/Poppins-Bold.ttf")
+  );
   const doc = new pdfDoc({bufferPages: true});
   doc.pipe(res);
   // const stream = doc.pipe(blobStream());
   // doc.on('data',startCall)
   // doc.on('end',endCall)
-  doc.font(poppinsFont);
-  doc.fontSize(12).text("GOVERMENT OF GOA", {align: "center"});
-  doc.moveDown();
-  doc.fontSize(14).text("DIRECTORATE OF PLANNING,STATISTIC AND EVALUATION", {
+  doc
+    .font(poppinsFont)
+    .rect(20, 20, doc.page.width - 40, doc.page.height - 40)
+    .stroke();
+  const govtlogo = await axios.get(
+    "https://upload.wikimedia.org/wikipedia/en/8/8a/Seal_of_Goa.png",
+    {responseType: "arraybuffer"}
+  );
+  doc.image(govtlogo.data, 25, 25, {
+    fit: [100, 100],
     align: "center",
+    valign: "center",
   });
-  doc.moveDown();
-  doc.fontSize(13).text("Form No.9", {align: "center"});
-  doc.moveDown();
-  doc.fontSize(13).text("(see Rule 9)", {align: "center"});
-  doc.moveDown();
+
+  const birthdeathdeptlogo = await axios.get(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW8Lfahh7ZS8AYtyg8jhn64PT_W3RZm5hTm-pOMuZ6-9LEDl0Q6DT7iOacEwfglBZKszM&usqp=CAU",
+    {responseType: "arraybuffer"}
+  );
+  doc.image(birthdeathdeptlogo.data, doc.page.width - 125, 25, {
+    fit: [100, 100],
+    align: "center",
+    valign: "center",
+  });
+
   doc
+    .fontSize(12)
+    .text(konkani.statename, {align: "center"})
+    .text(english.statename, {align: "center"});
+  doc.moveDown();
+
+  doc
+    .fontSize(14)
+    .text(konkani.deptname, {
+      align: "center",
+    })
+    .text(english.deptname, {
+      align: "center",
+    });
+  doc.moveDown();
+
+  doc
+    .font(poppinsBoldFont)
+    .fontSize(13)
+    .text(konkani.birthcertificate, {align: "center"})
+    .text(english.birthcertificate, {align: "center"});
+  doc.moveDown();
+  doc.moveDown();
+
+  doc
+    .font(poppinsFont)
     .fontSize(11)
     .text(
-      `Register for: ${data.address} taluka:${data.muncipalityLocation} of district:North Goa`,
-      {align: `justify`}
+      `${konkani.name}/${english.name}: ${data.childFirstName} ${data.childLastName} `,
+      {
+        align: `justify`,
+      }
     );
   doc.moveDown();
-  doc.fontSize(11).text(`नाम:${data.childFirstName} ${data.childLastName} `, {
+
+  doc.text(`${konkani.sex}/${english.sex}: ${data.gender} `, {
     align: `justify`,
   });
   doc.moveDown();
+
+  doc.text(
+    `${konkani.dob}/${english.dob}: ${data.dateOfBirth.split("T")[0]} `,
+    {
+      align: `justify`,
+    }
+  );
+  doc.moveDown();
+
+  doc.text(`${konkani.nom}/${english.nom}: ${data.motherName} `, {
+    align: `justify`,
+  });
+  doc.moveDown();
+
+  doc.text(`${konkani.nof}/${english.nof}: ${data.fatherName} `, {
+    align: `justify`,
+  });
+  doc.moveDown();
+
+  doc.text(`${konkani.nogf}/${english.nogf}: ${data.grandFatherName} `, {
+    align: `justify`,
+  });
+  doc.moveDown();
+
+  doc.text(`${konkani.nogm}/${english.nogm}: ${data.grandMotherName} `, {
+    align: `justify`,
+  });
+  doc.moveDown();
+
+  doc.text(`${konkani.address}/${english.address}: ${data.address} `, {
+    align: `justify`,
+  });
+  doc.moveDown();
+
+  doc.text(`${konkani.regno}/${english.regno}: ${data.certiid}`, {
+    align: `justify`,
+  });
+  doc.text(
+    `${konkani.dor}/${
+      english.dor
+    }: ${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`,
+    {
+      align: `justify`,
+    }
+  );
+
+  doc.moveDown();
+  doc.moveDown();
+
+  const registrarsign = await axios.get(
+    "https://i.etsystatic.com/14605561/r/il/b28b7b/1923258197/il_570xN.1923258197_t7ei.jpg",
+    {responseType: "arraybuffer"}
+  );
+  doc.image(registrarsign.data, 50, doc.page.height - 200, {
+    fit: [100, 100],
+    align: "center",
+    valign: "center",
+  });
   doc
-    .fontSize(11)
-    .text(
-      `sex:${data.gender} Date of Birth:${
-        data.dateOfBirth.split("T")[0]
-      } Date of Registration:${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`,
-      {align: `justify`}
-    );
-  //`sex:Female DOB:20/10/2000  DOR:7/11/2000`,
-  doc.moveDown();
-  doc.fontSize(11).text(` Fathers name:${data.fatherName}`, {align: `justify`});
+    .fontSize(8)
+    .text(`${konkani.soi}/`, 70, doc.page.height - 120)
+    .text(`${english.soi}`)
+    .text(`${konkani.aoi}/`)
+    .text(`${english.aoi}`);
 
-  doc.moveDown();
-  doc.fontSize(11).text(` Mothers name:${data.motherName}`, {align: `justify`});
-
-  doc.moveDown();
-  doc.fontSize(11).text(`Nationality of mother:${data.fatherNationality}`, {
-    align: `justify`,
+  const stamp = await axios.get(
+    "https://image.shutterstock.com/z/stock-vector-blue-municipal-distressed-rubber-stamp-with-grunge-texture-1209316840.jpg",
+    {responseType: "arraybuffer"}
+  );
+  doc.image(stamp.data, doc.page.width / 2 - 50, doc.page.height - 140, {
+    fit: [100, 100],
+    align: "center",
+    valign: "center",
   });
 
-  doc.moveDown();
-  doc.fontSize(11).text(`Nationality of Father:${data.motherNationality}`, {
-    align: `justify`,
-  });
-
-  doc.moveDown();
-  doc.moveDown();
-  //  Nationality of father:Indian
-  const signature = await axios.get(
+  const chiefregistrar = await axios.get(
     "https://image.shutterstock.com/image-vector/autograph-hand-drawn-handwritten-signature-260nw-2055413072.jpg",
     {responseType: "arraybuffer"}
   );
-
-  doc.image(signature.data, {
-    width: 50,
-    height: 50,
-    align: "left",
-    valign: "center",
-  });
-  doc.moveDown();
-  doc.fontSize(11).text("signature of Authority", {align: "left"});
-  const stamp = await axios.get(
-    "https://static6.depositphotos.com/1003153/570/v/950/depositphotos_5705314-stock-illustration-certified-stamp.jpg",
-    {responseType: "arraybuffer"}
-  );
-  doc.image(stamp.data, {
-    width: 150,
-    height: 150,
+  doc.image(chiefregistrar.data, doc.page.width - 150, doc.page.height - 200, {
+    fit: [80, 80],
     align: "center",
     valign: "center",
   });
+  doc
+    .text(
+      `${konkani.cr}/${english.cr}`,
+      doc.page.width - 150,
+      doc.page.height - 140
+    )
+    .text(`${konkani.fs}/${english.fs}`);
 
   doc.end();
 };
---
+++ pages/api/pdf_getter/data/english.js
@@ -0,0 +1,23 @@
+const data = {
+  statename: "GOVERMENT OF GOA",
+  deptname: "DIRECTORATE OF PLANNING,STATISTIC AND EVALUATION",
+  birthcertificate: "BIRTH CERTIFICATE",
+  name: "Name",
+  sex: "Sex",
+  dob: "Date of Birth",
+  pob: "Place of Birth",
+  nom: "Name of Mother",
+  nof: "Name of Father",
+  nogf: "Name of Grandfather(Father's Side)",
+  nogm: "Name of Grandmother(Father's Side)",
+  address: "Address",
+  regno: "Registeration No",
+  dor: "Date of Registeration",
+  doi: "Date of Issue",
+  soi: "Signature of Issuing Authority",
+  aoi: "Address of Issuing Authority",
+  cr: "Chief Registrar",
+  fs: "Facsimile Signature",
+};
+
+export default data;
