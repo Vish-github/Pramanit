a067e107f3da4fb072be054e8c2da44801deeaa0
Shivam Naik <shivamnaik8239@gmail.com>
2022-06-30 19:22:27 +0530

    Fix: display death certificates on clicking view all applications

--- layout/ViewMoreHeader.jsx
+++ layout/ViewMoreHeader.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import styles from "../styles/ViewMoreHeader.module.css";
 
-function ViewMoreHeader({ title, type }) {
+function ViewMoreHeader({ title, type, certificate }) {
   return (
     <div className={styles.view_more_header_container}>
       <h2>{title}</h2>
-      <Link href={`/view_all_applications?type=${type}`}>
+      <Link
+        href={`/view_all_applications?type=${type}&certificate=${certificate}`}
+      >
         <button>View All</button>
       </Link>
     </div>
